7f0a88a430e0aac6218c1786d054ad5933e4cb10
abbemad <abbemad@gmail.com>
2018-12-21 05:52:38 +0100

    patch remove

--- Routes/movieRoutes.js
+++ Routes/movieRoutes.js
@@ -27,7 +27,6 @@ let routes = function (Movie){
                     }
             });
     });
-
     movieRouter.route('/:movieId')
         .options(function(err,res){
             res.header('Access-Control-Allow-Methods', 'GET,PUT,PATCH,DELETE,OPTIONS');
@@ -38,7 +37,7 @@ let routes = function (Movie){
 
         .get(function(req,res){
             // let home = 'http://localhost:8000/api/movies/';
-            let home = 'http://stud.hosted.hr.nl/0920859/fullstack/';
+            let home = 'https://stud.hosted.hr.nl/0920859/fullstack/';
             let reqMovie = req.movie.toJSON();
             reqMovie._links = {
                 self: {
@@ -53,36 +52,39 @@ let routes = function (Movie){
             
         })
 
-        .put(function(req,res){
-            req.movie.title = req.body.title;
-            req.movie.actor = req.body.actor;
-            req.movie.genre = req.body.genre;
-            
-            req.movie.save (function(err){
-                if(err)
-                    res.status(500).send(err);
-                else{
-                    res.json(req.movie);
-                }  
-            });
-        })
-        
-        .patch(function(req,res){
-            if(req.body._id)
-                delete req.body._id;
-
-            for(let p in req.body){
-                req.movie[p] = req.body[p];
+        .put(function (req, res) {
+            if (!req.body.title || !req.body.actor || !req.body.genre) {
+                res.status(418).json({message: 'cannot leave anything empty'});
+            } else {
+                req.movie.title = req.body.title;
+                req.movie.actor = req.body.actor;
+                req.movie.genre = req.body.genre;
+                req.movie.save(function (err) {
+                    if (err)
+                        res.status(500).send(err);
+                    else {
+                        res.json(req.movie);
+                    }
+                });
             }
+})
+        
+        // .patch(function(req,res){
+        //     if(req.body._id)
+        //         delete req.body._id;
 
-            req.movie.save(function(err){
-                if(err)
-                    res.status(500).send(err);
-                else{
-                    res.json(req.movie);
-                }  
-            });
-        })
+        //     for(let p in req.body){
+        //         req.movie[p] = req.body[p];
+        //     }
+
+        //     req.movie.save(function(err){
+        //         if(err)
+        //             res.status(500).send(err);
+        //         else{
+        //             res.json(req.movie);
+        //         }  
+        //     });
+        // })
         .delete(function(req,res){
             req.movie.remove(function(err){
                 if (err)
